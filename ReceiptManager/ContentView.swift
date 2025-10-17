f99af8c5f77ed20505a9f478badddcd3d258edca
BronGLL <93700612+BronGLL@users.noreply.github.com>
2025-10-17 12:00:42 -0700

    Initial Commit

---
+++ ReceiptManager/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ReceiptManager
+//
+//  Created by Bronsen Laine-Lasala on 10/17/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ReceiptManager/ReceiptManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReceiptManagerApp.swift
+//  ReceiptManager
+//
+//  Created by Bronsen Laine-Lasala on 10/17/25.
+//
+
+import SwiftUI
+
+@main
+struct ReceiptManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
