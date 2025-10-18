730e3a86a0203f24e02837dde93c9c1dd52b461a
BronGLL <93700612+BronGLL@users.noreply.github.com>
2025-10-17 17:37:11 -0700

    Test Commit

--- ReceiptManagerIOSApp/ReceiptManagerIOSApp/ContentView.swift
+++ ReceiptManagerIOSApp/ReceiptManagerIOSApp/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Put it in Michael's backlog")
         }
         .padding()
     }
